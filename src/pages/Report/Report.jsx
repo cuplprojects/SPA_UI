9eaac3a989a947209745101c975fc75f608499d8
cuplprojects <madhavi@chandrakala.co.in>
2025-05-30 12:24:47 +0530

    Update Report.jsx

--- src/pages/Report/Report.jsx
+++ src/pages/Report/Report.jsx
@@ -10,13 +10,13 @@ const onChange = (key) => {
   console.log(key);
 };
 const Report = () => (
-  <Tabs onChange={onChange} type="card">
+ <Tabs onChange={onChange} type="card">
     <Tabs.TabPane tab="General Report" key="1">
       <ReportForm />
     </Tabs.TabPane>
-    {/* <Tabs.TabPane tab="Flag Report" key="2">
+    <Tabs.TabPane tab="Flag Report" key="2">
       <Flagreport/>
-    </Tabs.TabPane> */}
+    </Tabs.TabPane>
   </Tabs>
 );
 export default Report;
